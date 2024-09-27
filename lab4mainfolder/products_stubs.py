f2fa1b106a9282c990176f00c3bb9a8fdd76dc28
Your Name <yourname@example.com>
2024-09-27 22:22:28 +0500

    new file of price

--- lab4mainfolder/products_stubs.py
+++ lab4mainfolder/products_stubs.py
@@ -5,3 +5,22 @@ class Product:
       self.name = name
       self.amount = amount
       self.price = price
+#esha alvi sp23-bai-015  maintainer
+def get_price(self, quantity):
+    if quantity < 0:
+        raise ValueError("Quantity cannot be negative. Please use positive values only.")
+
+    if quantity < 10:
+        discount = 0
+    elif quantity < 100:
+        discount = 0.1
+    else:
+        discount = 0.2
+
+    total_price = self.price * quantity
+    discount_amount = total_price * discount
+    total_price -= discount_amount
+
+    return total_price
+
+     
