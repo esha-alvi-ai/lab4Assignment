4b561cf20e56224d5363a23f56888eaea9df5006
Cybil Fatima <cybil.fatima@gmail.com>
2024-09-27 22:38:57 +0500

    added purchase

--- lab4mainfolder/products_stubs.py
+++ lab4mainfolder/products_stubs.py
@@ -21,6 +21,24 @@ def get_price(self, quantity):
     discount_amount = total_price * discount
     total_price -= discount_amount
 
+    return total_price# coded by cybil fatima sp23-bai-013  collaborator
+
+
+def make_purchase(self, quantity):
+    if quantity < 0:
+        raise ValueError("Quantity cannot be negative. Please put a positive number.")
+    if quantity > self.amount:
+        raise ValueError("Cannot purchase more items than are in stock.")
+
+    total_price = self.get_price(quantity)
+    self.amount -= quantity  
+
+    print(str(quantity) + " " + self.name + "(s) purchased for $" + format(total_price, ".2f") + ".")
+    print("Stock left: " + str(self.amount))
+    
     return total_price
+
+
+
 
      
