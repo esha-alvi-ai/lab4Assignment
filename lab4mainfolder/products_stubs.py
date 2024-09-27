4ce78dab78a136b5d16eb1923af8bea95a3a7cbf
Your Name <yourname@example.com>
2024-09-27 19:34:03 +0500

    this is main for coded file

---
+++ lab4mainfolder/products_stubs.py
@@ -0,0 +1,7 @@
+#esha alvi sp23-bai-015  maintainer 
+#cybil fatima sp23-bai-013 collaborator
+class Product:
+  def __init__(self, name, amount, price):
+      self.name = name
+      self.amount = amount
+      self.price = price
